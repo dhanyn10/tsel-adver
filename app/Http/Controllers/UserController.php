cf9cec79bd2eefcf7d41cc960abc407b061fa1a6
dhanyn10 <dhanyn.dhan@gmail.com>
2022-11-15 19:27:17 +0700

    update user crud: showAll, ById, create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,58 +4,77 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Validator;
 
 class UserController extends Controller
 {
-    public function showRegister()
+    public function showAll()
     {
-        return view('register');
+        $data = User::all();
+        if(count($data) > 0) {
+            return response()->json($data, 200);
+        } else {
+            return response()->json([
+                'message' => 'data not exist'
+            ], 404);
+        }
+    }
+    public function showById(Request $request)
+    {
+        $id = $request->id;
+        $data = User::where('id', $id)->get();
+
+        $code = 0;
+        $ms = null;
+        if(count($data) > 0) {
+            return response()->json($data, $code);
+        } else {
+
+        return response()->json([
+            'message' => 'data not found'
+        ], 404);
+        }
     }
 
-    public function register(Request $request)
+    public function create(Request $request)
     {
+        $validasi = Validator::make($request->all(), [
+            'nama'  => 'required',
+            'email' => 'required|email',
+            'telepon' => 'numeric|digits_between:11,13'
+        ]);
+
+        if($validasi->fails()) {
+            return response()->json(['message' => 'wrong input'], 405);
+        }
+        
         $nama = $request->nama;
         $email = $request->email;
         $telepon = $request->telepon;
-        $user = User::where('email', $email)->get();
 
-        if(count($user) > 0) {
-            $response = array(
-                'msg'   => 0
-            );
-        } else {
+        $code = 0;
+        $ms = null;
+        $cekdata = User::where('email', $email)->get();
+        if(count($cekdata) == 0)
+        {
             $create = User::create([
                 'nama' => $nama,
                 'email' => $email,
                 'telepon' => $telepon
             ]);
 
-            $response = array(
-                'msg'   => 1
-            );
+            if($create) {
+                $code = 200;
+                $ms = "data created";
+            } else {
+                $code = 405;
+                $ms = "failed creating data";
+            }
+        } else {
+            $code = 405;
+            $ms = "data already exists";
         }
-        return response()->json($response);
-    }
 
-    public function showLogin()
-    {
-        return view('login');
-    }
-
-    public function login(Request $request)
-    {
-        $email = $request->email;
-        $user = User::where('email', $email)->get();
-
-        $code = 0;
-        $nama = null;
-        if(count($user) > 0) {
-            $nama = $user->pluck('nama')->first();
-            $code = 1;
-        }
-        return response()->json([
-            'code' => $code,
-            'nama' => $nama
-        ]);
+        return response()->json(['message' => $ms], $code);
     }
 }
