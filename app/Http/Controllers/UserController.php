eea368d53ff4f1df111cbd3c6a0ec40063abcef6
dhanyn10 <dhanyn.dhan@gmail.com>
2022-11-15 17:21:19 +0700

    register prosesor

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -13,6 +14,26 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        return \Response::json($request);
+        $nama = $request->nama;
+        $email = $request->email;
+        $telepon = $request->telepon;
+        $user = User::where('email', $email)->get();
+
+        if(count($user) > 0) {
+            $response = array(
+                'msg'   => 0
+            );
+        } else {
+            $create = User::create([
+                'nama' => $nama,
+                'email' => $email,
+                'telepon' => $telepon
+            ]);
+
+            $response = array(
+                'msg'   => 1
+            );
+        }
+        return response()->json($response);
     }
 }
