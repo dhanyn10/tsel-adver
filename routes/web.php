04f8d41551a3ee1a13feb7eda94b486c8778eca3
dhanyn10 <dhanyn.dhan@gmail.com>
2022-11-15 19:27:29 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DasborController;
 
 Route::get('/', function () {
     return redirect()->route('register');
 });
-Route::get('register', [UserController::class, 'showRegister'])->name('register');
-Route::get('login', [UserController::class, 'showLogin'])->name('login');
+Route::get('register', [RegisterController::class, 'show'])->name('register');
+Route::get('login', [LoginController::class, 'show'])->name('login');
 Route::get('dasbor', [DasborController::class, 'show'])->name('dasbor');
 
 Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
+Route::post('login', [LoginController::class, 'login']);
